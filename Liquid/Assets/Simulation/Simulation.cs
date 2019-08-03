d5f1559e72ad721e3115fd5d16c6bfe9a91a0660
Pablo <pablo.bollansee@gmail.com>
2019-08-03 16:16:15 +0200

    Nicer main scene

--- Liquid/Assets/Simulation/Simulation.cs
+++ Liquid/Assets/Simulation/Simulation.cs
@@ -3,245 +3,245 @@ using UnityEngine.Assertions;
 
 public class Simulation : MonoBehaviour
 {
-	//
-	// Settings
-	// ---
-	[Header("Sim Settings")]
-	[SerializeField]
-	int _gridPixelCount = 1024;
-	public int GridPixelCount { get { return _gridPixelCount; } }
+    //
+    // Settings
+    // ---
+    [Header("Sim Settings")]
+    [SerializeField]
+    int _gridPixelCount = 1024;
+    public int GridPixelCount { get { return _gridPixelCount; } }
 
-	[SerializeField]
-	float _updateInterval = 0.01f; // also called deltaTime in the paper, denoted "_DT" in shaders
-	public float UpdateInterval { get { return _updateInterval; } }
+    [SerializeField]
+    float _updateInterval = 0.01f; // also called deltaTime in the paper, denoted "_DT" in shaders
+    public float UpdateInterval { get { return _updateInterval; } }
 
-	[SerializeField]
-	float _gridPixelSize = 0.1f; // also called pipe-length (l) in the paper, denoted "_L" in shaders
-	public float GridPixelSize { get { return _gridPixelSize; } }
-	[SerializeField]
-	float _pipeCrossSectionArea = 0.1f;
-	[SerializeField]
-	float _gravityConstant = 9.81f;
+    [SerializeField]
+    float _gridPixelSize = 0.1f; // also called pipe-length (l) in the paper, denoted "_L" in shaders
+    public float GridPixelSize { get { return _gridPixelSize; } }
+    [SerializeField]
+    float _pipeCrossSectionArea = 0.1f;
+    [SerializeField]
+    float _gravityConstant = 9.81f;
 
-	[Header("Sand Settings")]
-	[SerializeField]
-	float _sandBlurPerSecond = 10.0f;
-	[SerializeField]
-	float _sandBlurAtStartSigma = 7.0f;
+    [Header("Sand Settings")]
+    [SerializeField]
+    float _sandBlurPerSecond = 10.0f;
+    [SerializeField]
+    float _sandBlurAtStartSigma = 7.0f;
 
-	[Header("Erosion Settings")]
-	[SerializeField]
-	float _sedimentCapacityConstant = 1;
-	public float SedimentCapacityConstant { get { return _sedimentCapacityConstant; } }
-	[SerializeField]
-	float _dissolvingConstant = 0.01f;
-	[SerializeField]
-	float _depositionConstant = 0.01f;
-	[SerializeField]
-	float _erosionMinimumAngleThresshold = 0.001f;
-	public float ErosionMinimumAngleThresshold { get { return _erosionMinimumAngleThresshold; } }
+    [Header("Erosion Settings")]
+    [SerializeField]
+    float _sedimentCapacityConstant = 1;
+    public float SedimentCapacityConstant { get { return _sedimentCapacityConstant; } }
+    [SerializeField]
+    float _dissolvingConstant = 0.01f;
+    [SerializeField]
+    float _depositionConstant = 0.01f;
+    [SerializeField]
+    float _erosionMinimumAngleThresshold = 0.001f;
+    public float ErosionMinimumAngleThresshold { get { return _erosionMinimumAngleThresshold; } }
 
-	[Header("Initializaton")]
-	[SerializeField]
-	Material _initialHeights;
+    [Header("Initializaton")]
+    [SerializeField]
+    Material _initialHeights;
 
-	[Header("Performace")]
-	[SerializeField]
-	int _maxSimStepsPerFrame = 5;
+    [Header("Performace")]
+    [SerializeField]
+    int _maxSimStepsPerFrame = 5;
 
-	//
-	// Schaders
-	// ---
-	[Header("Simulation Shaders")]
-	[SerializeField]
-	Shader _updateOutflowFluxShader;
-	[SerializeField]
-	Shader _updateHeightsShader;
-	[SerializeField]
-	Shader _updateVelocityFieldShader;
-	[SerializeField]
-	Shader _updateErosionDepositionShader;
-	[SerializeField]
-	Shader _updateSedimentTransportationShader;
-	[SerializeField]
-	Shader _blurSandShader;
+    //
+    // Schaders
+    // ---
+    [Header("Simulation Shaders")]
+    [SerializeField]
+    Shader _updateOutflowFluxShader;
+    [SerializeField]
+    Shader _updateHeightsShader;
+    [SerializeField]
+    Shader _updateVelocityFieldShader;
+    [SerializeField]
+    Shader _updateErosionDepositionShader;
+    [SerializeField]
+    Shader _updateSedimentTransportationShader;
+    [SerializeField]
+    Shader _blurSandShader;
 
-	//
-	// Materials
-	// ---
-	Material _updateOutflowFluxMaterial;
-	Material _updateHeightsMaterial;
-	Material _updateVelocityFieldMaterial;
-	Material _updateErosionDepositionMaterial;
-	Material _updateSedimentTransportationMaterial;
-	Material _blurSandMaterial;
+    //
+    // Materials
+    // ---
+    Material _updateOutflowFluxMaterial;
+    Material _updateHeightsMaterial;
+    Material _updateVelocityFieldMaterial;
+    Material _updateErosionDepositionMaterial;
+    Material _updateSedimentTransportationMaterial;
+    Material _blurSandMaterial;
 
-	//
-	// Textures
-	// ---
-	BufferedRenderTexture _waterSandRockSediment; // R: water, G: sand, B: rock, A: sediment
-	public RenderTexture CurrentWaterSandRockSediment { get { return _waterSandRockSediment.Texture; } }
-	BufferedRenderTexture _outflowFluxRLBT; // outflowflux R: right, G: left, B: bottom, A: top
-	public RenderTexture CurrentOutflowFluxRLBT { get { return _outflowFluxRLBT.Texture; } }
-	BufferedRenderTexture _velocityXY; //velocity: R: x, G: y
-	public RenderTexture CurrentVelocityXY { get { return _velocityXY.Texture; } }
+    //
+    // Textures
+    // ---
+    BufferedRenderTexture _waterSandRockSediment; // R: water, G: sand, B: rock, A: sediment
+    public RenderTexture CurrentWaterSandRockSediment { get { return _waterSandRockSediment.Texture; } }
+    BufferedRenderTexture _outflowFluxRLBT; // outflowflux R: right, G: left, B: bottom, A: top
+    public RenderTexture CurrentOutflowFluxRLBT { get { return _outflowFluxRLBT.Texture; } }
+    BufferedRenderTexture _velocityXY; //velocity: R: x, G: y
+    public RenderTexture CurrentVelocityXY { get { return _velocityXY.Texture; } }
 
-	//
-	// Other
-	// ---
-	float _nextUpdate;
+    //
+    // Other
+    // ---
+    float _nextUpdate;
 
-	//
-	// Events
-	// ---
-	public event System.Action OnBeforeSimFrame;
-	public event System.Action OnSimStep;
-	public event System.Action OnAfterSimFrame;
+    //
+    // Events
+    // ---
+    public event System.Action OnBeforeSimFrame;
+    public event System.Action OnSimStep;
+    public event System.Action OnAfterSimFrame;
 
-	//
-	// Code
-	// ---
-	void Start ()
-	{
-		// Some assurances
-		Assert.IsFalse(_initialHeights == null, "Missing initial heights material."); //IsNotNull doesn't work for some reason
+    //
+    // Code
+    // ---
+    void Start()
+    {
+        // Some assurances
+        Assert.IsFalse(_initialHeights == null, "Missing initial heights material."); //IsNotNull doesn't work for some reason
 
-		// Create materials
-		_updateOutflowFluxMaterial = new Material(_updateOutflowFluxShader);
-		_updateHeightsMaterial = new Material(_updateHeightsShader);
-		_updateVelocityFieldMaterial = new Material(_updateVelocityFieldShader);
-		_updateErosionDepositionMaterial = new Material(_updateErosionDepositionShader);
-		_updateSedimentTransportationMaterial = new Material(_updateSedimentTransportationShader);
-		_blurSandMaterial = new Material(_blurSandShader);
+        // Create materials
+        _updateOutflowFluxMaterial = new Material(_updateOutflowFluxShader);
+        _updateHeightsMaterial = new Material(_updateHeightsShader);
+        _updateVelocityFieldMaterial = new Material(_updateVelocityFieldShader);
+        _updateErosionDepositionMaterial = new Material(_updateErosionDepositionShader);
+        _updateSedimentTransportationMaterial = new Material(_updateSedimentTransportationShader);
+        _blurSandMaterial = new Material(_blurSandShader);
 
-		// Create textures
-		_gridPixelCount = Mathf.ClosestPowerOfTwo(_gridPixelCount);
-		var format = RenderTextureFormat.ARGBFloat;
-		var readWrite = RenderTextureReadWrite.Linear;
-		Assert.IsTrue(SystemInfo.SupportsRenderTextureFormat(format), "Rendertexture format not supported: " + format);
-		_waterSandRockSediment = new BufferedRenderTexture(_gridPixelCount, _gridPixelCount, 0, format, readWrite, _initialHeights);
-		_outflowFluxRLBT = new BufferedRenderTexture(_gridPixelCount, _gridPixelCount, 0, format, readWrite, Texture2D.blackTexture);
-		_velocityXY = new BufferedRenderTexture(_gridPixelCount, _gridPixelCount, 0, format, readWrite, Texture2D.blackTexture);
+        // Create textures
+        _gridPixelCount = Mathf.ClosestPowerOfTwo(_gridPixelCount);
+        var format = RenderTextureFormat.ARGBFloat;
+        var readWrite = RenderTextureReadWrite.Linear;
+        Assert.IsTrue(SystemInfo.SupportsRenderTextureFormat(format), "Rendertexture format not supported: " + format);
+        _waterSandRockSediment = new BufferedRenderTexture(_gridPixelCount, _gridPixelCount, 0, format, readWrite, _initialHeights);
+        _outflowFluxRLBT = new BufferedRenderTexture(_gridPixelCount, _gridPixelCount, 0, format, readWrite, Texture2D.blackTexture);
+        _velocityXY = new BufferedRenderTexture(_gridPixelCount, _gridPixelCount, 0, format, readWrite, Texture2D.blackTexture);
 
-		// Blur sand a bit
-		BlurSandOnce();
+        // Blur sand a bit
+        BlurSandOnce();
 
-		// Start first simulation step
-		_nextUpdate = Time.time;
-	}
-	
-	void Update ()
-	{
-		int iter = 0;
-		if (OnBeforeSimFrame != null) OnBeforeSimFrame();
-		while (Time.time >= _nextUpdate && iter < _maxSimStepsPerFrame)
-		{
-			UpdateSimulation();
-			_nextUpdate += _updateInterval;
-			iter++;
-			if (OnSimStep != null) OnSimStep();
-		}
-		if (OnAfterSimFrame != null) OnAfterSimFrame();
-	}
+        // Start first simulation step
+        _nextUpdate = Time.time;
+    }
 
-	void UpdateSimulation()
-	{
-		// Do all steps
-		UpdateFluxStep();
-		UpdateHeightsStep();
-		UpdateVelocityXY();
-		UpdateErosionDeposition();
-		UpdateSedimentTransportation();
-	}
+    void Update()
+    {
+        int iter = 0;
+        if (OnBeforeSimFrame != null) OnBeforeSimFrame();
+        while (Time.time >= _nextUpdate && iter < _maxSimStepsPerFrame)
+        {
+            UpdateSimulation();
+            _nextUpdate += _updateInterval;
+            iter++;
+            if (OnSimStep != null) OnSimStep();
+        }
+        if (OnAfterSimFrame != null) OnAfterSimFrame();
+    }
 
-	void UpdateFluxStep()
-	{
-		// Set values
-		_updateOutflowFluxMaterial.SetTexture("_WaterSandRockSedimentTex", _waterSandRockSediment.Texture);
-		_updateOutflowFluxMaterial.SetFloat("_DT", _updateInterval);
-		_updateOutflowFluxMaterial.SetFloat("_L", _gridPixelSize);
-		_updateOutflowFluxMaterial.SetFloat("_A", _pipeCrossSectionArea);
-		_updateOutflowFluxMaterial.SetFloat("_G", _gravityConstant);
+    void UpdateSimulation()
+    {
+        // Do all steps
+        UpdateFluxStep();
+        UpdateHeightsStep();
+        UpdateVelocityXY();
+        UpdateErosionDeposition();
+        UpdateSedimentTransportation();
+    }
 
-		// Do the step
-		Graphics.Blit(_outflowFluxRLBT.Texture, _outflowFluxRLBT.Buffer, _updateOutflowFluxMaterial);
+    void UpdateFluxStep()
+    {
+        // Set values
+        _updateOutflowFluxMaterial.SetTexture("_WaterSandRockSedimentTex", _waterSandRockSediment.Texture);
+        _updateOutflowFluxMaterial.SetFloat("_DT", _updateInterval);
+        _updateOutflowFluxMaterial.SetFloat("_L", _gridPixelSize);
+        _updateOutflowFluxMaterial.SetFloat("_A", _pipeCrossSectionArea);
+        _updateOutflowFluxMaterial.SetFloat("_G", _gravityConstant);
 
-		// Finalize
-		_outflowFluxRLBT.Swap();
-	}
+        // Do the step
+        Graphics.Blit(_outflowFluxRLBT.Texture, _outflowFluxRLBT.Buffer, _updateOutflowFluxMaterial);
 
-	void UpdateHeightsStep()
-	{
-		// Set values
-		_updateHeightsMaterial.SetTexture("_OutflowFluxRLBT", _outflowFluxRLBT.Texture);
-		_updateHeightsMaterial.SetFloat("_DT", _updateInterval);
-		_updateHeightsMaterial.SetFloat("_L", _gridPixelSize);
-		_updateHeightsMaterial.SetFloat("_SandBlurPerSecond", _sandBlurPerSecond);
+        // Finalize
+        _outflowFluxRLBT.Swap();
+    }
 
-		// Do the step
-		Graphics.Blit(_waterSandRockSediment.Texture, _waterSandRockSediment.Buffer, _updateHeightsMaterial);
+    void UpdateHeightsStep()
+    {
+        // Set values
+        _updateHeightsMaterial.SetTexture("_OutflowFluxRLBT", _outflowFluxRLBT.Texture);
+        _updateHeightsMaterial.SetFloat("_DT", _updateInterval);
+        _updateHeightsMaterial.SetFloat("_L", _gridPixelSize);
+        _updateHeightsMaterial.SetFloat("_SandBlurPerSecond", _sandBlurPerSecond);
 
-		// Finalize
-		_waterSandRockSediment.Swap();
-	}
+        // Do the step
+        Graphics.Blit(_waterSandRockSediment.Texture, _waterSandRockSediment.Buffer, _updateHeightsMaterial);
 
-	void UpdateVelocityXY()
-	{
-		// Set values
-		_updateVelocityFieldMaterial.SetTexture("_OutflowFluxRLBT", _outflowFluxRLBT.Texture);
-		_updateVelocityFieldMaterial.SetTexture("_WaterSandRockSedimentTex", _waterSandRockSediment.Texture);
-		_updateVelocityFieldMaterial.SetTexture("_PreviousWaterSandRockSedimentTex", _waterSandRockSediment.Buffer);
-		_updateVelocityFieldMaterial.SetFloat("_L", _gridPixelSize);
+        // Finalize
+        _waterSandRockSediment.Swap();
+    }
 
-		// Do the step
-		Graphics.Blit(_velocityXY.Texture, _velocityXY.Buffer, _updateVelocityFieldMaterial);
+    void UpdateVelocityXY()
+    {
+        // Set values
+        _updateVelocityFieldMaterial.SetTexture("_OutflowFluxRLBT", _outflowFluxRLBT.Texture);
+        _updateVelocityFieldMaterial.SetTexture("_WaterSandRockSedimentTex", _waterSandRockSediment.Texture);
+        _updateVelocityFieldMaterial.SetTexture("_PreviousWaterSandRockSedimentTex", _waterSandRockSediment.Buffer);
+        _updateVelocityFieldMaterial.SetFloat("_L", _gridPixelSize);
 
-		// Finalize
-		_velocityXY.Swap();
-	}
+        // Do the step
+        Graphics.Blit(_velocityXY.Texture, _velocityXY.Buffer, _updateVelocityFieldMaterial);
 
-	void UpdateErosionDeposition()
-	{
-		// Set values
-		_updateErosionDepositionMaterial.SetTexture("_VelocityXY", _velocityXY.Texture);
+        // Finalize
+        _velocityXY.Swap();
+    }
 
-		_updateErosionDepositionMaterial.SetFloat("_DT", _updateInterval);
-		_updateErosionDepositionMaterial.SetFloat("_L", _gridPixelSize);
+    void UpdateErosionDeposition()
+    {
+        // Set values
+        _updateErosionDepositionMaterial.SetTexture("_VelocityXY", _velocityXY.Texture);
 
-		_updateErosionDepositionMaterial.SetFloat("_Kc", _sedimentCapacityConstant);
-		_updateErosionDepositionMaterial.SetFloat("_Ks", _dissolvingConstant);
-		_updateErosionDepositionMaterial.SetFloat("_Kd", _depositionConstant);
-		_updateErosionDepositionMaterial.SetFloat("_ErosionMinimumAngleThresshold", _erosionMinimumAngleThresshold);
+        _updateErosionDepositionMaterial.SetFloat("_DT", _updateInterval);
+        _updateErosionDepositionMaterial.SetFloat("_L", _gridPixelSize);
 
-		// Do the step
-		Graphics.Blit(_waterSandRockSediment.Texture, _waterSandRockSediment.Buffer, _updateErosionDepositionMaterial);
+        _updateErosionDepositionMaterial.SetFloat("_Kc", _sedimentCapacityConstant);
+        _updateErosionDepositionMaterial.SetFloat("_Ks", _dissolvingConstant);
+        _updateErosionDepositionMaterial.SetFloat("_Kd", _depositionConstant);
+        _updateErosionDepositionMaterial.SetFloat("_ErosionMinimumAngleThresshold", _erosionMinimumAngleThresshold);
 
-		// Finalize
-		_waterSandRockSediment.Swap();
-	}
+        // Do the step
+        Graphics.Blit(_waterSandRockSediment.Texture, _waterSandRockSediment.Buffer, _updateErosionDepositionMaterial);
 
-	void UpdateSedimentTransportation()
-	{
-		_updateSedimentTransportationMaterial.SetTexture("_VelocityXY", _velocityXY.Texture);
-		_updateSedimentTransportationMaterial.SetFloat("_DT", _updateInterval);
-		_updateSedimentTransportationMaterial.SetFloat("_L", _gridPixelSize);
+        // Finalize
+        _waterSandRockSediment.Swap();
+    }
 
-		// Do the step
-		Graphics.Blit(_waterSandRockSediment.Texture, _waterSandRockSediment.Buffer, _updateSedimentTransportationMaterial);
+    void UpdateSedimentTransportation()
+    {
+        _updateSedimentTransportationMaterial.SetTexture("_VelocityXY", _velocityXY.Texture);
+        _updateSedimentTransportationMaterial.SetFloat("_DT", _updateInterval);
+        _updateSedimentTransportationMaterial.SetFloat("_L", _gridPixelSize);
 
-		// Finalize
-		_waterSandRockSediment.Swap();
-	}
+        // Do the step
+        Graphics.Blit(_waterSandRockSediment.Texture, _waterSandRockSediment.Buffer, _updateSedimentTransportationMaterial);
 
-	void BlurSandOnce()
-	{
-		// Set values
-		_blurSandMaterial.SetFloat("_Sigma", _sandBlurAtStartSigma);
+        // Finalize
+        _waterSandRockSediment.Swap();
+    }
 
-		// Do the step
-		Graphics.Blit(_waterSandRockSediment.Texture, _waterSandRockSediment.Buffer, _blurSandMaterial);
+    void BlurSandOnce()
+    {
+        // Set values
+        _blurSandMaterial.SetFloat("_Sigma", _sandBlurAtStartSigma);
 
-		// Finalize
-		_waterSandRockSediment.Swap();
-	}
+        // Do the step
+        Graphics.Blit(_waterSandRockSediment.Texture, _waterSandRockSediment.Buffer, _blurSandMaterial);
+
+        // Finalize
+        _waterSandRockSediment.Swap();
+    }
 }
